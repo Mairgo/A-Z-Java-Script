41f31211aa4c3110dbbb527096995db3f24a6827
Mairgo <121745384+Mairgo@users.noreply.github.com>
2024-06-30 14:38:53 +0000

    variable config done

---
+++ JS_Basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 1234
+let accountEmail = "basics01@gmail.com"
+var accountPass = "1212"
+accountCity = "New Delhi"
+
+let accountState;
+
+/*
+There are basically 3 ways for naming a variable: by using the keyword "Let"(mostly used). 
+Followed by var(mostly not preferred)
+prefer not to use var bcz of issue in block scope and functional scope
+and one directly without any keyword.
+
+Also a variable can remain unintialised and the output will be undefined
+*/
+
+console.table([accountId,accountEmail,accountPass,accountCity,accountState])
+
