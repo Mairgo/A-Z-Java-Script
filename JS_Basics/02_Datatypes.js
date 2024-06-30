29eb61d6f0d6917da170836fc21c2974d7fd2eec
Mairgo <121745384+Mairgo@users.noreply.github.com>
2024-06-30 17:30:45 +0000

    datatype config done

---
+++ JS_Basics/02_Datatypes.js
@@ -0,0 +1,21 @@
+ let name = "Ashutosh"
+ let age = 18
+ let isLoggedIn = true
+
+ console.log(typeof "Ashutsoh")
+ console.log(typeof 18)
+ console.log(typeof isLoggedIn)
+ console.log(typeof null)
+ console.log(typeof undefined)
+
+ /* 
+number= 2 to the pow of 53
+bigint
+string=" "
+boolean=true/false
+null= stand alone value, also treated as an object(often referred to as the language's drawback)
+undefined = undefined only(return type)
+symbol = unique (to find the uniqueness)
+
+undefined itself is a type but null is treated as an object
+ */
